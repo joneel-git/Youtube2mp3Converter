e6add2ff75507c14e9a64f54f8c28704cce4ed92
joneel-git <johnj.es@outlook.com>
2025-01-17 18:33:00 +0100

    Youtube2mp3 pytube dropped + YT-dlp

--- functions.py
+++ functions.py
@@ -1,42 +1,66 @@
-from pytube import YouTube
-from pytubefix import YouTube
-from pytubefix.cli import on_progress
-from CTkMessagebox import CTkMessagebox
-from customtkinter import CTkButton, CTk
-from pathlib import Path
+import re  
+from pytube import YouTube 
+import yt_dlp
 import os
+import sys
+from pathlib import Path
+from CTkMessagebox import CTkMessagebox
 
 
 def path_to_images(image_name):
-    # Get the current working directory
-    image_dir = os.getcwd()
+    # For PyInstaller, use _MEIPASS to get the path when bundled
+    if hasattr(sys, '_MEIPASS'):
+        image_dir = sys._MEIPASS
+    else:
+        image_dir = os.getcwd()
+    
     # The full path to the image
     images_path = os.path.join(image_dir, "images", image_name)
     return images_path
 
 
 def get_url_from(entry):
+    print("Function called")
     path_to_download = str(os.path.join(Path.home(), "Downloads"))
     save_path = path_to_download
     url = entry.get()  # Get the URL from the entry widget
+    print("URL:", url)
+
+    # Validate the YouTube URL
+    if not re.match(r"^(https?://)?(www\.)?(youtube\.com|youtu\.be)/.+$", url):
+        CTkMessagebox(
+            title="Error",
+            message="Please enter a valid YouTube URL.",
+            icon=path_to_images("error.png"),
+        )
+        return None
+
     try:
-        # This is the youtube object
-        yt = YouTube(url)
-        # Attempt to get highest resolution video or audio only
-        ys = yt.streams.filter(only_audio=True).first()
-        # Set the filename to the video title with .mp3 extension
-        filename = f"{yt.title}.mp3"
-        # Download the audio stream
-        ys.download(output_path=save_path, filename=filename)
+        # Set options for yt-dlp
+        ydl_opts = {
+            "format": "bestaudio/best",
+            "postprocessors": [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "mp3",
+                    "preferredquality": "192",
+                }
+            ],
+            "outtmpl": os.path.join(save_path, "%(title)s.%(ext)s"),
+        }
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
 
         CTkMessagebox(
             title="Success",
             message="Download completed successfully!",
             icon=path_to_images("check.png"),
         )
-        return ys
+        return None
 
     except Exception as e:
+        print("Error occurred:", e)
         CTkMessagebox(
             title="Error",
             message=f"An error has occurred: {e}",
