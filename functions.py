1a3643743a98c13bce4cf2b3a2bd48142947c345
joneel-git <johnj.es@outlook.com>
2025-02-15 15:51:30 +0100

    Youtube2mp3 Grid

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
 
+
 def checkbox_event(x):
     # Access the value of the associated variable directly
     current_value = x.get()  # Assuming check_var is accessible here
@@ -8,9 +9,11 @@ def checkbox_event(x):
     else:
         print("Checkbox is off, current value: ", current_value)
 
-def entry_input(my_entry, listbox):
+
+def entry_input(my_entry, frame_below):
     value = my_entry.get()
     collect_data = [value]
     # listbox.insert(END, value)
     for x in collect_data:
-        listbox.insert(END, x)
+        frame_below.insert("end", x)
+
--- main.py
+++ main.py
@@ -9,39 +9,38 @@ ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("dark-blue")
 
 
-class App(ctk.CTk):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.title("Grid")  # Use self to refer to the current instance
-        self.geometry("700x500")
-        # ////////////// Main Window Frame ///////////////
-        self.my_frame = ctk.CTkFrame(
-            master=self  # This is where am setting main frame to the root of the program
-        )
-        self.my_frame.grid()
+class MyFrame(ctk.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
 
-        self.my_frame.grid_rowconfigure(
-            0, weight=0
-        )  # Here we have configured 2 rows to use
-        self.my_frame.grid_rowconfigure(1, weight=1)
-        self.my_frame.grid_columnconfigure(
-            0, weight=1
-        )  # Here we have configured 2 columns to use
-        self.my_frame.grid_columnconfigure(1, weight=1)
-
-        # Widgets
+        # add widgets onto the frame...
         self.label = ctk.CTkLabel(
-            self.my_frame,
+            master=self,
             text="Iam a Label",
         )
-        self.button1 = ctk.CTkButton(self.my_frame, text="Button1")
-        self.my_entry = ctk.CTkEntry(self.my_frame, width=500)
+        # ///////////////////////////////////////////////
+        self.frame_below = CTkListbox(
+            master=self,
+            border_color="gray",
+            border_width=1,
+            width=600,
+            height=200,
+        )
+        # //////// the Entry widget
+        self.my_entry = ctk.CTkEntry(master=self, width=500)
+
+        # //////// This Submit button handles the calls from entry
+        self.button1 = ctk.CTkButton(
+            master=self,
+            command=lambda: entry_input(self.my_entry, self.frame_below),
+            text="Submit",
+        )
 
         self.x = ctk.IntVar()
 
         # Create the checkbox
         self.checkbox = ctk.CTkCheckBox(
-            self.my_frame,
+            master=self,
             text="CTkCheckBox",
             command=lambda: checkbox_event(self.x),
             variable=self.x,
@@ -50,20 +49,29 @@ class App(ctk.CTk):
         )
         self.checkbox.grid()
 
-        self.second_frame = ctk.CTkFrame(master=self)
-        # Placing widgets using Grid
+        # ///////// Placing widgets using Grid
         self.label.grid(row=1, column=0, padx=10, pady=10)
         self.my_entry.grid(row=2, column=0, padx=10, pady=10)
         self.checkbox.grid(row=2, column=1, padx=20, pady=10)
         self.button1.grid(row=3, column=0, padx=10, pady=10)
-        self.second_frame.grid(row=0, column=2, sticky=("N", "S", "E"))
+        self.frame_below.grid(row=4, column=0, padx=20, pady=20, sticky="ew")
 
-        # ////////////// List Box frame ///////////////
+
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("Grid")  # Use self to refer to the current instance
+        self.geometry("800x400")
+        # This is placing the class MyFrame above on to the root of the app
+        self.my_frame = MyFrame(master=self)
+        self.my_frame.grid()
+        self.my_frame.grid_rowconfigure(0, weight=0)
+        self.my_frame.grid_rowconfigure(1, weight=1)
+        self.my_frame.grid_columnconfigure(0, weight=1)
+        self.my_frame.grid_columnconfigure(1, weight=1)
 
 
 if __name__ == "__main__":
     app = App()
     # This runs the app
     app.mainloop()
-
-# Build 2 frames
